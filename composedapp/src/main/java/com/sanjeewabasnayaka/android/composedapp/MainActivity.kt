a66093dfbc153a910bd7607513c5731ca43555e3
Hiransanjeewa <hiransanjeewaa@gmail.com>
2023-05-16 14:14:33 +0530

    New input fields added , Name and age Next - adding unit test s and validations

--- composedapp/src/main/java/com/sanjeewabasnayaka/android/composedapp/MainActivity.kt
+++ composedapp/src/main/java/com/sanjeewabasnayaka/android/composedapp/MainActivity.kt
@@ -17,6 +17,7 @@ import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
 import com.sanjeewabasnayaka.android.composedapp.services.Calculate
+import com.sanjeewabasnayaka.android.composedapp.services.HellomessageService
 import com.sanjeewabasnayaka.android.composedapp.ui.theme.Android1Theme
 
 class MainActivity : ComponentActivity() {
@@ -25,63 +26,96 @@ class MainActivity : ComponentActivity() {
         setContent {
             Android1Theme {
                 // A surface container using the 'background' color from the theme
-                Twogreetings()
+                //  Twogreetings()
 
             }
         }
     }
 }
 
+// **********    Addition of two numbers    **************
+
+//@Composable
+//fun Twogreetings(){
+//    Surface(
+//        modifier = Modifier.fillMaxSize(),
+//        color = MaterialTheme.colors.background
+//    ) {
+//        Column{
+//           // Greeting("Hello Hiran")
+//           // Greeting("Hello Android")
+//            var text1 by remember { mutableStateOf(TextFieldValue("0", TextRange(0,7))) }
+//            var text2 by remember { mutableStateOf(TextFieldValue("0", TextRange(0,7))) }
+//            var total by remember {
+//                mutableStateOf(0)
+//            }
+//            val calculate = Calculate();
+//
+//            TextField(
+//                value = text1, onValueChange = { text1 = it },
+//                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+//                label = {Text("number 1")}
+//            )
+//            TextField(
+//                value = text2, onValueChange = { text2 = it },
+//                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+//                label = {Text("number 2")}
+//            )
+//
+//            Button(onClick = {
+//                total=calculate.add(text1.text.toInt(),text2.text.toInt())
+//            Log.d("total","Total is "+total)
+//            }
+//            ) {
+//                 Text(text = "Click here")
+//            }
+//            Text(text = "Total is : "+ total)
+//        }
+//    }
+//}
+
 @Composable
 fun Twogreetings(){
-
     Surface(
-
         modifier = Modifier.fillMaxSize(),
         color = MaterialTheme.colors.background
     ) {
         Column{
-           // Greeting("Hello Hiran")
-           // Greeting("Hello Android")
-            var text1 by remember { mutableStateOf(TextFieldValue("0", TextRange(0,7))) }
-            var text2 by remember { mutableStateOf(TextFieldValue("0", TextRange(0,7))) }
-            var total by remember {
-                mutableStateOf(0)
-            }
-            val calculate = Calculate();
+            // Greeting("Hello Hiran")
+            // Greeting("Hello Android")
+            var name by remember { mutableStateOf(TextFieldValue(" ", TextRange(0,7))) }
+            var age by remember { mutableStateOf(TextFieldValue(" ", TextRange(0,7))) }
 
+            var message=" "
+
+            val helloMessageService=HellomessageService()
             TextField(
-                value = text1, onValueChange = { text1 = it },
-                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
-                label = {Text("number 1")}
+                value = name, onValueChange = { name = it },
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text),
+                label = {Text("Name")}
             )
             TextField(
-                value = text2, onValueChange = { text2 = it },
+                value = age, onValueChange = { age = it },
                 keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
-                label = {Text("number 2")}
-
+                label = {Text("Age")}
             )
 
             Button(onClick = {
-
-                total=calculate.add(text1.text.toInt(),text2.text.toInt())
-            Log.d("total","Total is "+total)
+                message=helloMessageService.showMessage(name.text,age.text)
+                Log.d("Hello Message",message)
             }
             ) {
-                 Text(text = "Click here")
+                Text(text = "Click here")
             }
-            Text(text = "Total is : "+ total)
+            Text(text = message)
         }
-
-
     }
 }
 
-@Composable
-fun Greeting(name: String) {
 
-    Text(text = "Hello $name!")
-}
+
+
+
 
 @Preview(showBackground = true)
 @Composable
---
+++ composedapp/src/main/java/com/sanjeewabasnayaka/android/composedapp/services/HellomessageService.kt
@@ -0,0 +1,7 @@
+package com.sanjeewabasnayaka.android.composedapp.services
+
+class HellomessageService {
+    fun showMessage(name:String,age:String): String {
+        return "Hello $name , You are $age years old."
+    }
+}
