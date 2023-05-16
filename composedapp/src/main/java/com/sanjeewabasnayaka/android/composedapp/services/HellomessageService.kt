1a6ff2f0e3c3686767f948f3850fa4ab4df16f2c
Hiransanjeewa <hiransanjeewaa@gmail.com>
2023-05-16 14:32:08 +0530

    Validations edited, Unit tests added

--- composedapp/src/main/java/com/sanjeewabasnayaka/android/composedapp/services/HellomessageService.kt
+++ composedapp/src/main/java/com/sanjeewabasnayaka/android/composedapp/services/HellomessageService.kt
@@ -2,13 +2,12 @@ package com.sanjeewabasnayaka.android.composedapp.services
 
 class HellomessageService {
     fun showMessage(name:String,age:String): String {
-        if (name==" "){
-            return "Name cannot be empty"
-        }else if (   age==" "){
-
-            return "Age cannot be empty"
+        return if (name==""){
+            "Name cannot be empty"
+        }else if (   age==""){
+            "Age cannot be empty"
         }else{
-            return "Hello "+name+ " , You are "+age+" years old."
+            "Hello "+name+ " , You are "+age+" years old."
         }
     }
 }
---
+++ composedapp/src/test/java/com/sanjeewabasnayaka/android/composedapp/HelloMessageServiceTests.kt
@@ -0,0 +1,29 @@
+package com.sanjeewabasnayaka.android.composedapp
+
+import com.sanjeewabasnayaka.android.composedapp.services.HellomessageService
+import org.junit.Assert
+import org.junit.Test
+
+class HelloMessageServiceTests {
+
+    @Test
+    fun it_should_return_an_error_if_name_is_empty(){
+        val hellomessageService= HellomessageService()
+        var result=hellomessageService.showMessage("","23")
+        Assert.assertEquals("Name cannot be empty",result)
+    }
+
+    @Test
+    fun it_should_return_an_error_if_age_is_empty(){
+        val hellomessageService= HellomessageService()
+        var result=hellomessageService.showMessage("Hiran","")
+        Assert.assertEquals("Age cannot be empty",result)
+    }
+
+    @Test
+    fun it_should_return_welcome_message_if_name_and_age_provided(){
+        val hellomessageService= HellomessageService()
+        var result=hellomessageService.showMessage("Hiran","23")
+        Assert.assertEquals("Hello Hiran , You are 23 years old.",result)
+    }
+}
